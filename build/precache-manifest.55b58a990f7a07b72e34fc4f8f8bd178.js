9b06068e6367fef721c069276650450638f97a3b
tsekylekobie <tse.kylekobie@berkeley.edu>
2020-05-18 16:09:15 -0700

    added server starter code

---
+++ build/precache-manifest.55b58a990f7a07b72e34fc4f8f8bd178.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "774a1d78b1d380b3660ec92a1b96bc8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c425d685b03840e320d9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b347a3de9fba5d18cfbd",
+    "url": "/static/js/2.e2fc00c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e2fc00c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c425d685b03840e320d9",
+    "url": "/static/js/main.6e2cbe47.chunk.js"
+  },
+  {
+    "revision": "d405d00bde28104a6fcc",
+    "url": "/static/js/runtime-main.3370808f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,30 @@
+// Dependencies
+var express = require("express");
+var http = require("http");
+var path = require("path");
+var socketIO = require("socket.io");
+
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+app.set("port", 8080);
+app.use(express.static(path.join(__dirname, "build")));
+
+// Routing
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+// Starts the server.
+server.listen(8080, function () {
+  console.log("Starting server on port 8080");
+});
+
+// Add the WebSocket handlers
+io.on("connection", function (socket) {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
